18f24b334d9213783126a9a1ed1e1cbe52b08020
Victor Jian Choe <vchoe89@gmail.com>
2019-07-22 22:09:22 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -1,25 +1,29 @@
 def create_an_empty_array
-  
+  []
 end
 
 def create_an_array
-  
+  array = ["Wow", "I", "Really", "Am"]
 end
 
 def add_element_to_end_of_array(array, element)
-  
+  array = ["one", "two", "three", "four"]
+  array << "arrays!"
 end
 
 def add_element_to_start_of_array(array, element)
-  
+  array = ["one", "two", "three", "four"]
+  array.unshift("wow")
 end
 
 def remove_element_from_end_of_array(array)
-  
+  array = ["I", "am", "really", "learning", "arrays!"]
+  array.pop
 end
 
 def remove_element_from_start_of_array(array)
-  
+  array = ["wow", "am", "really", "learning", "arrays!"]
+  array.shift
 end
 
 def retrieve_element_from_index(array, index_number)
